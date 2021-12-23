410528fdb4d2640d07f1029cc7b33bbf8cecf4fc
Giulio <giuliohome@gmail.com>
2021-12-23 12:01:08 +0100

    C++ arrow operator

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,14 +5,14 @@
 
 // from https://forum.qt.io/topic/17971/qtableview-doesn-t-show-any-data/3
 // The problem is that you are creating the model on the stack. As soon as pModel goes out of scope, it is destroyed again.
-MyModel myModel;
+MyModel *myModel = new MyModel;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->tableView->setModel(&myModel);
+    ui->tableView->setModel(myModel);
     qDebug() << "model set!";
 }
 
@@ -25,7 +25,7 @@ int count = 0;
 void MainWindow::on_pushButton_clicked()
 {
     count++;
-    myModel.setCount(2 + count);
+    myModel->setCount(2 + count);
     ui->label->setText(("clicked " + std::to_string( count ) + "!").c_str());
 }
 
